1839ebfa6385c64077de9d075ccd9b1dc6d52957
etanha <e_tanha@yahoo.com>
2018-08-29 16:01:30 +0430

    first commit prj

---
+++ SanaApp/Assets/Script/Tokhmeh1/Story/Frame1/Tkhm1_Str1_Frm1_MovieHandler.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Tkhm1_Str1_Frm1_MovieHandler : MonoBehaviour {
+
+    //Raw Image to Show Video Images [Assign from the Editor]
+    public RawImage image;
+    //Video To Play [Assign from the Editor]
+    public VideoClip videoToPlay;
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+
+    //Audio
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+    }
+
+    IEnumerator playVideo()
+    {
+        //Add VideoPlayer to the GameObject
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        //Add AudioSource
+        audioSource = gameObject.AddComponent<AudioSource>();
+        
+        //Disable Play on Awake for both Video and Audio
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+
+        //We want to play from video clip not from url
+        videoPlayer.source = VideoSource.VideoClip;
+
+        //Set video To Play then prepare Audio to prevent Buffering
+        videoPlayer.clip = videoToPlay;
+        //Assign the Audio from Video to AudioSource to be played
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+        audioSource.volume = 0.03f;
+
+        //Set Audio Output to AudioSource
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        
+
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        while (!videoPlayer.isPrepared)
+        {
+            Debug.Log("Preparing Video");
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+
+     
+
+        //Assign the Audio from Video to AudioSource to be played
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        //Assign the Texture from Video to RawImage to be displayed
+        image.texture = videoPlayer.texture;
+
+        //Play Video
+        videoPlayer.Play();
+
+        //Play Sound
+        audioSource.Play();
+
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+
+        Debug.Log("Done Playing Video");
+    }
+
+
+
+
+
+
+    /* public MovieTexture movie;
+      AudioSource audioClip;
+      // Use this for initialization
+      void Start () {
+          GetComponent<RawImage>().texture = movie as MovieTexture;
+          audioClip = GetComponent<AudioSource>();
+          movie.Play();
+          audioClip.clip = movie.audioClip;
+          audioClip.Play();
+      }
+
+
+      // Update is called once per frame
+      void Update () {
+
+      }*/
+
+    /* private MovieTexture video;
+      VideoPlayer vid;
+      private AudioSource sound;
+
+      void Start()
+      {
+
+        //  video = GetComponent<VideoPlayer>();
+
+          sound = GetComponent<AudioSource>();
+          play();
+      }
+
+      private void play()
+      {
+          video.Play();
+          sound.Play();
+      }*/
+
+
+}
