16a8e9af3f23e02c3d526a3fd57a67f31b763104
mzlg60 <mzolghadr60@gmail.com>
2018-09-04 12:50:17 +0430

    liaghat va zolghadr taqir dadand.

---
+++ SanaApp/Assets/Script/Main/Main_LevelManagerUnLocked.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManagerUnLocked : MonoBehaviour {
+
+	[SerializeField]
+	private int level;
+
+	[SerializeField]
+	public Image Image; 
+	public GameObject scenePrefab;
+	public GameObject scenePrefabLoad;
+	public GameObject Can_Menu;
+
+	void Start () {
+		if (PlayerPrefs.GetInt ("Level") >= level) {
+			Image.enabled = false;
+		}
+	}
+
+	public void LevelSelect(){
+		Destroy(Can_Menu);
+		scenePrefabLoad = Instantiate (scenePrefab) as GameObject;
+	}
+}
---
+++ SanaApp/Assets/Script/Tokhmeh1/Story/Frame2/Tkhm1_Str1_Frm2_PlayStory.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tkhm1_Str1_Frm2_PlayStory : MonoBehaviour {
+	public AudioSource audioSource;
+	public AudioClip audioClipStory;
+	public AudioClip audioClipQuestion;
+	public AudioClip audioClipAlternativTokhme;
+	public AudioClip audioClipAlternativBazi;
+
+	public GameObject GOAlternativeBubbleTokhme;
+	public GameObject GOAlternativeBubbleBazi;
+
+	public bool isZoomed=false;
+	public Animator panelZoomAnim;
+
+
+	void Start () {
+		//play story
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.time = 48f;
+		audioClipStory = audioSource.clip;
+		audioSource.Play ();
+
+		//invisible the alternatives
+		GOAlternativeBubbleTokhme.SetActive (false);
+		GOAlternativeBubbleBazi.SetActive (false);
+
+		//TV is not zoomed
+		isZoomed = false;
+		panelZoomAnim.SetBool ("isZoomed", false);
+
+	}
+	
+
+
+	void Update () {
+		if (audioSource.time > 95f) {
+			audioSource.Stop ();
+			StartCoroutine (PlayAudioClipQuestion ());
+		}
+	}
+
+
+	IEnumerator PlayAudioClipQuestion()
+	{
+
+		isZoomed = true;
+		panelZoomAnim.SetBool ("isZoomed", true);
+
+		audioSource.PlayOneShot (audioClipQuestion);
+		yield return new WaitForSeconds (audioClipQuestion.length);
+		StartCoroutine (PlayAudioClipAlternativTokhme ());
+	}
+
+
+	IEnumerator PlayAudioClipAlternativTokhme()
+	{
+		GOAlternativeBubbleTokhme.SetActive (true);
+		audioSource.PlayOneShot (audioClipAlternativTokhme);
+		yield return new WaitForSeconds (audioClipAlternativTokhme.length);
+		StartCoroutine (PlayAudioClipAlternativBazi ());
+	}
+
+
+	IEnumerator PlayAudioClipAlternativBazi()
+	{
+		GOAlternativeBubbleBazi.SetActive (true);
+		audioSource.PlayOneShot (audioClipAlternativBazi);
+		yield return new WaitForSeconds (audioClipAlternativBazi.length);
+	}
+}
