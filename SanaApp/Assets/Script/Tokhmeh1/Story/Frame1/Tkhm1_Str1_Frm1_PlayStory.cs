1a1c47ff09a9a064467999c6548b9edf2c094ee8
zliaghatgit <zliaghatgit>
2018-09-30 17:56:37 +0330

    seday frame hay 1,2,3,3-1 & taghier tabligh

--- SanaApp/Assets/Script/Tokhmeh1/Story/Frame1/Tkhm1_Str1_Frm1_PlayStory.cs
+++ SanaApp/Assets/Script/Tokhmeh1/Story/Frame1/Tkhm1_Str1_Frm1_PlayStory.cs
@@ -7,18 +7,17 @@ public class Tkhm1_Str1_Frm1_PlayStory : MonoBehaviour {
 	public AudioSource audioSource;
 	public AudioClip audioClipStoryFrm1;
 
-	//public Animator[] Animations;
 
-	// Use this for initialization
 	void Start () {
-		//for(int i=0; i<Animations;)
+		
 		audioSource = GetComponent<AudioSource> ();
 
+		audioSource.time =3.5f;
 		audioClipStoryFrm1 = audioSource.clip;
 		audioSource.Play();
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		if (audioSource.time>48f)
 			audioSource.Stop ();
---
+++ SanaApp/Assets/Script/Tokhmeh1/Story/Frame3/Tkhm1_Str1_Frm3_PlayStory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tkhm1_Str1_Frm3_PlayStory : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public AudioClip audioClipStoryFrm3;
+
+
+	void Start () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+
+		audioClipStoryFrm3 = audioSource.clip;
+		audioSource.Play();
+	}
+	
+
+	void Update () {
+	//stop dar payan bakhsh alef az frame 3
+		if (audioSource.time>34f)
+			audioSource.Stop ();
+	}
+}
